308b456de4eac1f646fc98149688b814e7c976fc
tip14 <alexeykopyev@gmail.com>
2021-10-29 21:45:40 +0200

    connect to mongodb in app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,8 +1,17 @@
+const config = require('./utils/config')
+const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
-
 const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,9 +1,5 @@
 const config = require('../utils/config')
-
 const mongoose = require('mongoose')
-const mongoUrl = config.MONGODB_URI
-
-mongoose.connect(mongoUrl)
 
 const blogSchema = new mongoose.Schema({
   title: String,
