0d5106a2898dcc1a453ea4e1468806a319949aaa
tip14 <alexeykopyev@gmail.com>
2021-10-29 21:43:28 +0200

    user blogsRouter

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,28 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app;
