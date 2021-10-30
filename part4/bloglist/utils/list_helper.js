c8bf1b1823727406fd4d32687db502b1718d5b70
tip14 <alexeykopyev@gmail.com>
2021-10-30 09:17:51 +0200

    helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,6 +6,17 @@ const totalLikes = (blogs) => {
     return blogs.reduce((total, curr) => total + curr.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (!blogs || blogs.length == 0) return {}
+
+    const sorted = Array.from(blogs).sort((a, b) => b.likes - a.likes)
+    const favorite = sorted[0];
+    delete favorite._id
+    delete favorite.__v
+    delete favorite.url
+    return favorite
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
