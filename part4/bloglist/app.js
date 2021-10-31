d6d92cc1a493c6813720f883acf9cf08c12ab37c
tip14 <alexeykopyev@gmail.com>
2021-10-31 16:40:41 +0100

    bloglist expansion, step6

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const app = express()
 
 mongoose.connect(config.MONGODB_URI)
@@ -18,5 +19,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app;
