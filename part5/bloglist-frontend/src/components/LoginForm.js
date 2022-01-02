4d085f54e25fc82ea39341d05ca53319a087413c
tip14 <alexeykopyev@gmail.com>
2022-01-02 21:22:24 +0100

    bloglist end to end testing, step1

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -15,8 +15,8 @@ const LoginForm = ({ onSubmitLoginForm, username, password, onUsernameChange, on
     return (
 
         <form>
-            <p>username <input type="text" value={username} onChange={onUsernameChange} /></p>
-            <p>password <input type="password" value={password} onChange={onPasswordChange} /></p>
+            <p>username <input id="username" type="text" value={username} onChange={onUsernameChange} /></p>
+            <p>password <input id="password" type="password" value={password} onChange={onPasswordChange} /></p>
             <input type="submit" onClick={onSubmitLoginForm} value="login" />
         </form>
     )
