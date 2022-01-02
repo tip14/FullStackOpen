b46cc5a1d5df871f94b8d39e454aac28e472203a
tip14 <alexeykopyev@gmail.com>
2022-01-02 21:35:40 +0100

    bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/integration/bloglist.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist.spec.js
@@ -11,3 +11,30 @@ describe('Blog app', function() {
         cy.get('input[type=submit]')
     })
 })
+
+
+describe('Login',function() {
+
+    const username = 'tuser'
+    const password = 'tpass123'
+
+    beforeEach(function() {
+        cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        cy.request('POST', 'http://localhost:3003/api/users', {username, password})
+        cy.visit('http://localhost:3000')
+    })
+
+    it('succeeds with correct credentials', function() {
+        cy.get('#username').type(username)
+        cy.get('#password').type(password)
+        cy.get('input[type=submit]').click()
+        cy.contains('logged in')
+    })
+
+    it.only('fails with wrong credentials', function() {
+        cy.get('#username').type(username)
+        cy.get('#password').type("78687")
+        cy.get('input[type=submit]').click()
+        cy.contains('wrong username or password').should('have.css', 'border', '2px solid rgb(255, 0, 0)')
+    })
+})
