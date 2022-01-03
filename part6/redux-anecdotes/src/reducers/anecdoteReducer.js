744d18a29eb297a9c02419d23f54efd37465a79b
tip14 <alexeykopyev@gmail.com>
2022-01-03 22:39:42 +0100

    anecdotes, step3

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,41 +1,60 @@
 const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
+    return {
+        content: anecdote,
+        id: getId(),
+        votes: 0
+    }
+}
+
+const sortByVotesDesc = (first, second) => {
+
+  console.log('sor')
+
+    if (first.votes > second.votes) {
+        return -1
+    }
+
+    if (first.votes < second.votes) {
+        return 1
+    }
+
+    return 0
 }
 
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+    console.log('state now: ', state)
+    console.log('action', action)
 
-  if (action.type === 'VOTE') {
-    return state.map(a => {
-      if (a.id === action.data.id) {
-        a.votes = ++a.votes
-      }
+    let newState;
 
-      return a
-    })
-  } else if (action.type === 'ADD') {
-    return state.concat(asObject(action.data.anecdote))
-  }
+    if (action.type === 'VOTE') {
+        newState = state.map(a => {
+            if (a.id === action.data.id) {
+                a.votes = ++a.votes
+            }
 
-  return state;
+            return a
+        })
+    } else if (action.type === 'ADD') {
+      newState = state.concat(asObject(action.data.anecdote));
+    } else {
+      newState = state
+    }
+
+    return newState.sort(sortByVotesDesc);
 }
 
 export default reducer
